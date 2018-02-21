597984fed32e177fbc29e048671f4f7f92b00d87
chhayaer@gmail.com <chhayaer@gmail.com>
2018-02-21 11:04:35 +0530

    updated code for simple SQLite

--- app/src/main/java/com/example/dell/myapplication/MainActivity.java
+++ app/src/main/java/com/example/dell/myapplication/MainActivity.java
@@ -6,33 +6,47 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TableLayout;
 
 public class MainActivity extends Activity {
 
+    TableLayout TabLayout_Show;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DatabaseHandler db = new DatabaseHandler(this);
+        final DatabaseHandler db = new DatabaseHandler(this);
+
+        TabLayout_Show = (TableLayout) findViewById(R.id.TableLayout2);
 
         // Inserting Contacts
         Log.d("Insert: ", "Inserting ..");
-        db.addContact(new Contact("Ravi", "9100000000"));
-        db.addContact(new Contact("Srinivas", "9199999999"));
-        db.addContact(new Contact("Tommy", "9522222222"));
-        db.addContact(new Contact("Karthik", "9533333333"));
-
-        // Reading all contacts
-        Log.d("Reading: ", "Reading all contacts..");
-        List<Contact> contacts = db.getAllContacts();
-
-        for (Contact cn : contacts) {
-            String log = "Id: "+cn.getID()+" ,Name: " + cn.getName() + " ,Phone: " +
-                    cn.getPhoneNumber();
-            // Writing Contacts to log
-            Log.d("Name: ", log);
+        if(db.getContactsCount()<=0) {
+            db.addContact(new Contact("Ravi", "9100000000"));
+            db .addContact(new Contact("Srinivas", "9199999999"));
+            db.addContact(new Contact("Tommy", "9522222222"));
+            db.addContact(new Contact("Karthik", "9533333333"));
         }
+
+        final Button button = findViewById(R.id.button_id);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Reading all contacts
+                Log.d("Reading: ", "Reading all contacts..");
+                List<Contact> contacts = db.getAllContacts();
+
+                for (Contact cn : contacts) {
+                    String log = "Id: "+cn.getID()+" ,Name: " + cn.getName() + " ,Phone: " +
+                            cn.getPhoneNumber();
+                    // Writing Contacts to log
+                    Log.d("Name: ", log);
+                }
+            }
+        });
     }
 
     @Override
